ffec3bcb411c612948099d9c7ab44deecbea087b
edwin <wangwensi@gmail.com>
2014-01-17 16:03:26 +0800

    company 1.17

--- src/main/java/com/thinkgem/jeesite/common/filter/PageCachingFilter.java
+++ src/main/java/com/thinkgem/jeesite/common/filter/PageCachingFilter.java
@@ -7,14 +7,15 @@ import net.sf.ehcache.constructs.web.filter.SimplePageCachingFilter;
 
 /**
  * 页面高速缓存过滤器
+ *
  * @author ThinkGem
  * @version 2013-8-5
  */
 public class PageCachingFilter extends SimplePageCachingFilter {
 
-	@Override
-	protected CacheManager getCacheManager() {
-		return CacheUtils.getCacheManager();
-	}
-	
+    @Override
+    protected CacheManager getCacheManager() {
+        return CacheUtils.getCacheManager();
+    }
+
 }
